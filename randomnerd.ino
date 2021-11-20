e2518ce50c070c2c964e578a56f4adc11d25826d
Dirk Luberth Dijkman <45427770+ldijkman@users.noreply.github.com>
2021-11-20 07:35:31 +0100

    Update randomnerd.ino

--- randomnerd.ino
+++ randomnerd.ino
@@ -2,7 +2,7 @@
 // espasyncwebserver maybe better
 // 
 // no hardcoding wifi settings and i/o => needs tzapu wifimanager with mdns name setting and i/o config and ntp server  and offset ntp
-// needs mdns name and list easy dot local url address like http://randomnerd.local = no hard IP numbers
+// needs mdns name and list easy dot local url address like http://randomnerd.local = no hard to lookup and remember IP numbers
 // needs easy read/set timers https://jsfiddle.net/luberth/h2tqgecy/show/
 
 
