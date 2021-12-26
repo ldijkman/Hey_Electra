84693d4f39d4443870ee3dc24ab2f441df4c7f4a
Dirk Luberth Dijkman <45427770+ldijkman@users.noreply.github.com>
2021-12-26 19:22:21 +0100

    Update ESP32_WiFi_Manager.ino

--- ESP32/RandomNerd/ESP32_WiFi_Manager.ino
+++ ESP32/RandomNerd/ESP32_WiFi_Manager.ino
@@ -310,7 +310,32 @@ void setup() {
 void loop() {
       browseService("http", "tcp");
     delay(5000);
+Serial.println("scan start");
 
+  // WiFi.scanNetworks will return the number of networks found
+  int n = WiFi.scanNetworks();
+  Serial.println("scan done");
+  if (n == 0) {
+      Serial.println("no networks found");
+  } else {
+    Serial.print(n);
+    Serial.println(" networks found");
+    for (int i = 0; i < n; ++i) {
+      // Print SSID and RSSI for each network found
+      Serial.print(i + 1);
+      Serial.print(": ");
+      Serial.print(WiFi.SSID(i));
+      Serial.print(" (");
+      Serial.print(WiFi.RSSI(i));
+      Serial.print(")");
+      Serial.println((WiFi.encryptionType(i) == WIFI_AUTH_OPEN)?" ":"*");
+      delay(10);
+    }
+  }
+  Serial.println("");
+
+  // Wait a bit before scanning again
+  delay(5000);
 }
 
 
