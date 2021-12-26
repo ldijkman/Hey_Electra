35fadd24ce7fd830a0679e84acc2f4f730df0a24
Dirk Luberth Dijkman <45427770+ldijkman@users.noreply.github.com>
2021-12-26 07:56:56 +0100

    Update ESP32_WiFi_Manager.ino

--- ESP32/RandomNerd/ESP32_WiFi_Manager.ino
+++ ESP32/RandomNerd/ESP32_WiFi_Manager.ino
@@ -18,9 +18,10 @@ http://electra.local
   wanted should show a scan to list all mdns devices dot local urls in local network automaticly on devices webpage
   wanted a settable countdown off timer
   wanted easy set/overview timed settings webpage in this device https://jsfiddle.net/luberth/ow3zceyn/show/
-  wanted dhcp ip settting not fixed
+  wanted dhcp ip settting not fixed  // people do not know wat to enter
   wanted unique Access point AP name broadcasted in the air == "esp32 wifimanager" + chipid
   wanted set mDNS dot local url from wifimanager inputfield
+  wanted page refresh to actual switch state if state is changed on another webpage
 *********/
 
 // https://github.com/ldijkman/Hey_Electra/blob/main/ESP32/RandomNerd/ESP32_WiFi_Manager.ino
