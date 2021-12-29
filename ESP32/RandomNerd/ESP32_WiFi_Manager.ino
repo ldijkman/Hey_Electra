517f3e1cd225f2341de0a6e6761aca5b3e860ea4
Dirk Luberth Dijkman <45427770+ldijkman@users.noreply.github.com>
2021-12-29 17:27:53 +0100

    Update ESP32_WiFi_Manager.ino

--- ESP32/RandomNerd/ESP32_WiFi_Manager.ino
+++ ESP32/RandomNerd/ESP32_WiFi_Manager.ino
@@ -16,8 +16,7 @@ http://electra.local
   Rui Santos
   Complete instructions at https://RandomNerdTutorials.com/esp32-wi-fi-manager-asyncwebserver/
 
-  Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files.
-  The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+  
 
 
   you need to upload the data directory to spiffs => Arduino IDE => Tools => ESP32 Sketch Data Upload (turn serial monitor off else failure)
